d7fa4c3e4ac14b3dee7124c8f1e4dc37837083af
ravimaharjan <ravi@homeloanexperts.com.au>
2020-05-17 16:30:42 +0545

    Use containers

--- quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/spiders/quotes_spider.py
@@ -1,17 +1,19 @@
 import scrapy
+from ..items import QuotetutorialItem
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
+        quote_items = QuotetutorialItem()
+
         all_quotes = response.css('div.quote')
         for quote in all_quotes:
             title = quote.css('span.text::text').extract()
             author = quote.css('.author::text').extract()
             tags = quote.css('.tag::text').extract()
-            yield {
-                    'title': title,
-                    'author': author,
-                    'tags': tags
-                 }
+            quote_items['title'] = title
+            quote_items['author'] = author
+            quote_items['tags'] = tags
+            yield quote_items
