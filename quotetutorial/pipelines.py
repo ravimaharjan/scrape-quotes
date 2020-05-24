bae686910914dac2c86de94a3b839bbe60fafa62
ravimaharjan <ravi@homeloanexperts.com.au>
2020-05-24 09:40:16 +0545

    pipeline for sqllite

--- quotetutorial/pipelines.py
+++ quotetutorial/pipelines.py
@@ -5,7 +5,33 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
 
 class QuotetutorialPipeline:
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect('quotes.db')
+        self.cursor = self.conn.cursor()
+    
+    def create_table(self):
+        self.cursor.execute(""" DROP TABLE IF EXISTS quotes_tb """)
+        self.cursor.execute(""" create table quotes_tb (
+            title text, author text, tags text ) """
+        )
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+    def store_db(self, item):
+        self.cursor.execute(""" insert into quotes_tb values (?,?,?) """, (
+            item['title'][0],
+            item['author'][0],
+            item['tags'][0]
+        ))
+        self.conn.commit()
+
